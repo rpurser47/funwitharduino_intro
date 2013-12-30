4c90b2c8f80792ef28da1eb075319bdbb8a8b926
rpurser47 <rob@purser.org>
2013-12-29 19:59:05 -0500

    Added Traffic Light Arrive -- Class 1 Done!

--- intro_class/class_1/Traffic_Light_Arrive/Traffic_Light_Arrive.ino
+++ intro_class/class_1/Traffic_Light_Arrive/Traffic_Light_Arrive.ino
@@ -1,7 +1,7 @@
 // Fun with Arduino by Rob Purser is licensed under a
 // Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
 // http://creativecommons.org/licenses/by-nc-sa/3.0/deed.en_US
-// Based on a work at  https://sites.google.com/site/funarduino/
+// Based on a work at https://sites.google.com/site/funarduino/
 // Copyright Rob Purser, 2013
 
 int signal1RedPin = 13;
@@ -10,6 +10,8 @@ int signal1GreenPin = 11;
 int signal2RedPin = 10;
 int signal2YellowPin = 9;
 int signal2GreenPin = 8;
+int arrivalSensorPin = 7;
+int delayTimingPin = 0;
 
 void setup()
 {
@@ -21,14 +23,22 @@ void setup()
   pinMode(signal2YellowPin,OUTPUT);
   pinMode(signal2GreenPin,OUTPUT);
   digitalWrite(signal2RedPin,HIGH);
+  
+  pinMode(arrivalSensorPin,INPUT_PULLUP);//Exercise 3
 }
 
 void loop()
 {
-  Serial.println("Green  Red");
+  Serial.println("Green  Red -- waiting on arrival");
   digitalWrite(signal1RedPin,LOW);
   digitalWrite(signal1GreenPin,HIGH);
-  delay(5000);
+
+  // Wait for the arrival sensor to go low
+  while(digitalRead(arrivalSensorPin) == HIGH)
+  {
+    delay(100);
+  }
+  Serial.println("Arrival Sensor Triggered");
   
   Serial.println("Yellow Red");
   digitalWrite(signal1GreenPin,LOW);
@@ -44,7 +54,7 @@ void loop()
   digitalWrite(signal2RedPin,LOW);
   digitalWrite(signal2GreenPin,HIGH);
   delay(5000);
-
+  
   Serial.println("Red    Yellow");
   digitalWrite(signal2GreenPin,LOW);
   digitalWrite(signal2YellowPin,HIGH);
