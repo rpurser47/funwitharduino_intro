037e01b388bb2c10d2d8af0d475c614a41b37ffc
Tom Amlicke <tamlicke@gmail.com>
2014-01-21 16:47:03 -0500

    H Bridge Fritzing and Arduino sketch

---
+++ class_4/H_Bridge/H_Bridge.ino
@@ -0,0 +1,34 @@
+// Fun with Arduino by Rob Purser is licensed under a
+// Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
+// http://creativecommons.org/licenses/by-nc-sa/3.0/deed.en_US
+// Based on a work at http://goo.gl/NNe35. 
+// Copyright Rob Purser, 2013
+
+int pinForward = 9;
+int pinReverse = 10;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(pinForward,OUTPUT);
+  pinMode(pinReverse,OUTPUT);
+}
+
+void loop()
+{
+  Serial.println("Forward");
+  analogWrite(pinForward,150);
+  delay(1000);
+
+  Serial.println("Free run");
+  analogWrite(pinForward,0);
+  delay(2000);
+
+  Serial.println("Reverse");
+  analogWrite(pinReverse,150);
+  delay(1000);
+
+  Serial.println("Free run");
+  analogWrite(pinReverse,0);
+  delay(2000);
+}
