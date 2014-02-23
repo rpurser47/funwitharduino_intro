6e8dfef84c8d78c3f115c689535ecced781a8436
Brian Bodiya <brian@bodiya.com>
2014-02-23 16:44:03 -0500

    removing lcd code

--- class_3/RGB_Serial/RGB_Serial.ino
+++ class_3/RGB_Serial/RGB_Serial.ino
@@ -4,20 +4,12 @@
 // Based on a work at http://goo.gl/NNe35. 
 // Copyright Rob Purser, 2013
 
-#include <LiquidCrystal.h>
-
-// initialize the library with the numbers of the interface pins
-LiquidCrystal lcd(12, 13, 5, 4, 3, 2);
-
 int pinsRGB[] = { 11, 10, 9};
 
 void setup()
 {
   Serial.begin(9600);
-  
-  // set up the LCD's number of columns and rows: 
-  lcd.begin(16, 2);
-  
+
   for(int iPin=0; iPin<3; iPin++)
   {
      pinMode(pinsRGB[iPin],OUTPUT);
@@ -31,19 +23,18 @@ void loop()
 {
   if(Serial.available() > 0)
   {
-    lcd.clear();
-    
     // Read the serial port
     int incomingByte = Serial.read();
     // Ignore anything except ASCII 48 to 57, '0' to '9'
     int command = constrain(incomingByte,48,57);
     // Translate '0' to 0, '9' to 255, etc.
-    lcd.print(command); lcd.print(" ");
     int output = map(command,48,57,0,255);
-    
+
+    Serial.print("Setting pin ");
+    Serial.print(pinsRGB[pinIndex]);
+    Serial.print(" to ");
+    Serial.println(output);
     analogWrite(pinsRGB[pinIndex],output);
-    lcd.print(pinIndex); lcd.print(" ");
     pinIndex = (pinIndex + 1) % 3;
-    lcd.print(output);lcd.print(" ");
   }
 }
